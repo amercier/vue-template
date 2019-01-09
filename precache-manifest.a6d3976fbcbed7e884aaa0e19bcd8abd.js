508c3a8e6589b3c426d26575b891a06a5b17e5fb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-09 11:22:54 +0000

    Deploy amercier/vue-template to github.com/amercier/vue-template.git:gh-pages

--- precache-manifest.a6d3976fbcbed7e884aaa0e19bcd8abd.js
+++ precache-manifest.a6d3976fbcbed7e884aaa0e19bcd8abd.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/js/chunk-vendors.66a834e9.js"
   },
   {
-    "revision": "75072018fb73266a1f15",
-    "url": "/js/app.e43f31ed.js"
+    "revision": "aabc3796c2c4762074fc",
+    "url": "/js/app.f21a2d43.js"
   },
   {
     "revision": "0f3c33cd3342b2770030",
     "url": "/js/about.d63ad26f.js"
   },
   {
-    "revision": "1f1eb15ab9545c4ad9880362362483ba",
+    "revision": "66b01f72ecad949eaadd99a6959534dd",
     "url": "/index.html"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/img/logo.82b9c7a5.png"
   },
   {
-    "revision": "75072018fb73266a1f15",
+    "revision": "aabc3796c2c4762074fc",
     "url": "/css/app.db75fe40.css"
   }
 ];
